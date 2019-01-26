962c59aaa223db22ca636d08e460bc23a8d9c881
Fallinger <ShutenkoIgor2016@yandex.ru>
2019-01-26 16:35:24 +0700

    Update DeformationMesh.cs

--- DeformationMesh.cs
+++ DeformationMesh.cs
@@ -13,7 +13,6 @@ public class DeformationMesh : MonoBehaviour
     public Slider RadiusSlider;
     public Slider PowerSlider;
 
-    // Приватные поля
     Color[] colors;
     Mesh mesh;
     Vector3[] vertices;
@@ -47,48 +46,40 @@ public class DeformationMesh : MonoBehaviour
 
     void Deform()
     {
-        // Изначальные настройки для устранения возможных ошибок
         if (isColorAccess)
         {
             colors = new Color[vertices.Length];
-            // Окрашивание всего меша в зелёный цвет
             for (int i = 0; i < vertices.Length; i++)
             {
                 colors[i] = Color.green;
             }
             isColorAccess = false;
         }
-        // Получение данных о вершинах и треугольниках меша
         vertices = mesh.vertices;
         triangles = mesh.triangles;
-        // Перебор всех вершин меша и манипуляции с ними
         for (int i = 0; i < vertices.Length; i++)
         {
             RaycastHit hit;
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             if (Physics.Raycast(ray, out hit))
             {
-                // Берём координаты по X
                 Vector3 vertX = new Vector3(vertices[i].x,0, 0);
                 Vector3 hitX = new Vector3(hit.point.x, 0, 0);
-                // Берём координаты по Z
+                
                 Vector3 vertZ = new Vector3(0, 0, vertices[i].z);
                 Vector3 hitZ = new Vector3(0, 0, hit.point.z);
-                // Находим ближайшие вершины к указателю мыши
-                // Сравнение по X
+
                 if (Vector3.Distance(vertX, hitX) <= RadiusSlider.value)
                 {
-                    // Сравнение по Z
                     if (Vector3.Distance(vertZ, hitZ) <= RadiusSlider.value)
                     {
-                        // Изменение цвета меша
                         if (GreenToggle.isOn)
                             colors[i] = Color.green;
                         if(YellowToggle.isOn)
                             colors[i] = Color.yellow;
                         if (BlueToggle.isOn)
                             colors[i] = Color.blue;
-                        // Деформация вершин по оси Y
+
                         if (UpDeformToggle.isOn)
                             vertices[i] += new Vector3(0, PowerSlider.value, 0) * Time.deltaTime;
                         else if (DownDeformToggle.isOn)
@@ -98,7 +89,6 @@ public class DeformationMesh : MonoBehaviour
                
 
             }
-            // Ограничение высоты вершин
             if (vertices[i].y > 1)
                 vertices[i].y = 1;
             if (vertices[i].y < -1)
